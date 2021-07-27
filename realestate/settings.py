5157be28d3e107929afbb1ae2fe5cea650912633
Mango Dev <ingridlewah@gmail.com>
2021-07-27 12:05:33 +0300

    fourth commit

--- realestate/settings.py
+++ realestate/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
